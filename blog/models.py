5f1e6460cb32b19dca66dbaf490993e6e408c38a
Rick-8 <npopbwls@students.codeinstitute.net>
2025-03-01 16:36:32 +0000

    Add - Admin settings and models

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
-
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
@@ -11,7 +10,7 @@ class Post(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts"
     )
-
     content = models.TextField()
+    excerpt = models.TextField(blank=True, null=True)  # Optional field
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
