eba81041541e7a0ef173a3ea0a3a2aa700f86792
Rick-8 <npopbwls@students.codeinstitute.net>
2025-03-06 13:26:33 +0000

    Debug Updated

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
